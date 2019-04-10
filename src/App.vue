cf06e3e04af7a56ec3d329e6c0f7832b4aa809ef
Ianko Djemerenov <iankodj@gmail.com>
2019-04-10 13:05:58 +0300

    chore

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,24 @@
 <template>
-  <div id="app">
-    <input v-model="firstQuery">
-    <input v-model="secondQuery">
-    <button @click="get">Get</button>
-    <br/>
-    <img :src="firstImageSrc" style="width:50%;" />
-    <img :src="secondImageSrc" style="width:50%;" />
+  <div id="wrapper" ref="mainWrapper">
+    <div id="app" class="image-overlay" :style="overlayStyle">
+      <a download :href="'data:text/plain;charset=utf-8,' + encodeURIComponent(text)">
+        <img :src="firstImageSrc" class="image-one-overlay"/>
+        <img :src="secondImageSrc" class="image-two-overlay"/>
+      </a>
+    </div>
+    <div>
+      <a href="#" @click.prevent="() => {showImages = !showImages}" style="color: black; opacity: .1;">{{showImages ? "hide" : "show"}}</a>
+      <div class="image-one" v-if="showImages">
+        <a :href="firstImageDownload"><img :src="firstImageSrc" /></a>
+        <br/>
+        Photo by <a target="_blank" :href="firstImageAttr + '?utm_source=' + appName + '&utm_medium=referral'">{{firstImageAuthor}}</a> on <a href="https://unsplash.com/?utm_source=DoubleExposure&utm_medium=referral">Unsplash</a>
+      </div>
+      <div class="image-two" v-if="showImages">
+        <a :href="secondImageDownload"><img :src="secondImageSrc" /></a>
+        <br/>
+        Photo by <a target="_blank" :href="secondImageAttr + '?utm_source=' + appName + '&utm_medium=referral'">{{secondImageAuthor}}</a> on <a href="https://unsplash.com/?utm_source=DoubleExposure&utm_medium=referral">Unsplash</a>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -16,27 +29,141 @@ export default {
   name: "App",
   data() {
     return {
-      firstQuery: "",
-      secondQuery: "",
-      firstImageSrc: "",
-      secondImageSrc: ""
+      firstImageSrcDefault: "",
+      secondImageSrcDefault: "",
+      overlayStyle: {
+        height: "800px",
+        width: "800px"
+      },
+      firstImage: null,
+      secondImage: null,
+      appName: "DoubleExposure",
+      alertPress: false,
+      intervalRunner: null,
+      images: [],
+      showImages: true
     };
   },
-  methods: {
-    get() {
-      this.$http.get('https://blend-service.azurewebsites.net/getrandomphoto?query=' + this.firstQuery).then((response) => {
-        var image = response.body;
-        this.firstImageSrc = image.urls.regular;
-      });
-
-      this.$http.get('https://blend-service.azurewebsites.net/getrandomphoto?query=' + this.secondQuery).then((response) => {
-        var image = response.body;
-        this.secondImageSrc = image.urls.regular;
-      });
+  computed: {
+    firstImageSrc () {
+      return this.firstImage ? this.firstImage.urls.regular : this.firstImageSrcDefault;
+    },
+    secondImageSrc () {
+      return this.secondImage ? this.secondImage.urls.regular : this.secondImageSrcDefault;
+    },
+    firstImageAttr () {
+      return this.firstImage ? this.firstImage.user.links.html : "";
+    },
+    secondImageAttr () {
+      return this.secondImage ? this.secondImage.user.links.html : "";
+    },
+    firstImageAuthor () {
+      return this.firstImage ? this.firstImage.user.name : "";
+    },
+    secondImageAuthor () {
+      return this.secondImage ? this.secondImage.user.name : "";
+    },
+    firstImageDownload () {
+      return this.firstImage ? this.firstImage.links.html : "";
+    },
+    secondImageDownload () {
+      return this.secondImage ? this.secondImage.links.html : "";
+    },
+    firstImageDirectDownload () {
+      return this.firstImage ? this.firstImage.links.download : "";
+    },
+    secondImageDirectDownload () {
+      return this.secondImage ? this.secondImage.links.download : "";
+    },
+    text () {
+      return this.firstImageAuthor ? `1 | Author: ${this.firstImageAuthor} | Insta: ${this.firstImage.user.instagram_username} | Download: ${this.firstImageDownload} | Direct Download: ${this.firstImageDirectDownload} \r\n
+2 | Author: ${this.secondImageAuthor} | Insta: ${this.secondImage.user.instagram_username} | Download: ${this.secondImageDownload} | Direct Download: ${this.secondImageDirectDownload}` : "";
     }
+  },
+  mounted () {
+    this.renderImages();
+    window.addEventListener('keyup', this.renderImages);
+  },
+  methods: {
+    renderImages () {
+      if(this.images.length) {
+        this.firstImage = this.images.pop();
+        this.secondImage = this.images.pop();
+        this.runInterval();
+      } else {
+        this.get();
+      }
+    },
+    get() {
+      this.$http.get('https://blend-service.azurewebsites.net' + '/getrandomphoto?count=30&orientation=squarish').then((response) => {
+        this.images = response.body;
+        this.renderImages();
+      }, (err) => {
+        alert(err.bodyText);
+        console.log(err.bodyText)
+      });
+    },
+    runInterval () {
+      window.clearInterval(this.intervalRunner);
+      this.intervalRunner = window.setInterval(this.renderImages, 20000);
+    },
+    getDetails () {
+     alert(1);
+    } 
   }
 };
 </script>
 
 <style>
+body {
+  background: #eee;
+}
+img {
+    height: 100%;
+}
+
+.alertPress {
+  position: absolute;
+  top: 0;
+  text-align: center;
+  width: 100%;
+  font-size: 32px;
+  background-color: #eee;
+}
+
+.image-overlay {
+    overflow: hidden;
+    position: relative;
+    width: 100%;
+    margin: 0 auto;
+}
+.image-one-overlay {
+    opacity: .5;
+}
+.image-two-overlay {
+    mix-blend-mode: overlay;
+    position: absolute;
+    top: 0;
+    left: 0;
+}
+
+#wrapper {
+  position: relative;
+}
+
+.image-one, .image-two {
+  position: absolute;
+  height: 200px;
+  width: 200px;
+}
+
+.image-one {
+  top: 0;
+  left: 0;
+} 
+
+.image-two {
+  top:250px;
+  left: 0;
+}
 </style>
